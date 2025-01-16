19d8e789cab153b0e2bf636b7e16c4416b34ca41
minjee <minjee3333@gmail.com>
2025-01-16 13:55:46 +0900

    coding test

---
+++ BaekJoon/2025.01.16/#3003_킹_퀸_룩_비숍_나이트_폰/Main3003.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main3003 {
+    public static void main(String[] args) throws IOException {
+        int[] basicArray = {1,1,2,2,2,8};
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputArray = br.readLine().split(" ");
+        br.close();
+
+        for (int i=0; i<basicArray.length; i++) {
+            System.out.print(basicArray[i] - Integer.parseInt(inputArray[i]) + " ");
+        }
+    }
+}
