c664bcb9e694dd2ce10b7bdb7fe1960b9a6df3ca
minjee <minjee3333@gmail.com>
2025-02-12 01:23:53 +0900

    #15894 수학은 체육과목 입니다 풀이

---
+++ BaekJoon/2025.02.12/#15894_수학은_체육과목_입니다/Java15894.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Java15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        sc.close();
+
+        System.out.println(n*4);
+    }
+}
