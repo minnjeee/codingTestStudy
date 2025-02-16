a6b8a2b36d3ea8e53b4517652aef62d2215ae2b8
minjee <minjee3333@gmail.com>
2025-02-16 19:23:01 +0900

    #24262, #24263 알고리즘의 수행시간 1,2 풀이

---
+++ BaekJoon/2025.02.16/#24263_알고리즘수업_알고리즘의_수행시간2/Java24263.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Java24263 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+        sc.close();
+
+        System.out.println(n);
+        System.out.println("1");
+    }
+}
